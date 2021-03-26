3a2a67993be9ef8eb8ba890b9ced8a1ba1d2eb58
neharadharapu <radharapuneha@gmail.com>
2021-03-26 23:56:27 +0530

    lcd is working

--- basic_lock.c
+++ basic_lock.c
@@ -1,65 +1,65 @@
 #include <xc.h>
+#include<htc.h>
+#define _XTAL_FREQ   20000000
 
-#define _XTAL_FREQ 20000000
+// BEGIN CONFIG
+#pragma config FOSC = HS // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON // Brown-out Reset Enable bit (BOR enabled)
+#pragma config CPD = OFF // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF // Flash  Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF // Flash Program Memory Code Protection bit (Code protection off)
+//END CONFIG
 
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+// Configuration word for PIC16F877A
+//__CONFIG( FOSC_HS & WDTE_OFF & PWRTE_ON & CP_OFF & BOREN_ON
+ //         & LVP_OFF & CPD_OFF & WRT_OFF & DEBUG_OFF);
 
-// defining macros
 
-#define RS PORTDbits.RD0
-#define RW PORTDbits.RD1
-#define EN PORTDbits.RD2
+// Define Pins
+#define LCD_E        RC1   // Enable pin for LCD
+#define LCD_RS       RC0   // RS pin for LCD
+#define LCD_Data_Bus   PORTB // Data bus for LCD
+// Define Pins direction register
+#define LCD_E_Dir         TRISC1
+#define LCD_RS_Dir        TRISC0
+#define LCD_Data_Bus_Dir   TRISB
+// Constants
+#define E_Delay       500
 
-#define C1 PORTBbits.RB0 //custom name basically
-#define C2 PORTBbits.RB1
-#define C3 PORTBbits.RB2
-#define R1 PORTBbits.RB4
-#define R2 PORTBbits.RB5
-#define R3 PORTBbits.RB6
-#define R4 PORTBbits.RB7
+#define C1 PORTDbits.RD0 //custom name basically
+#define C2 PORTDbits.RD1
+#define C3 PORTDbits.RD2
+#define R1 PORTDbits.RD3
+#define R2 PORTDbits.RD4
+#define R3 PORTDbits.RD5
+#define R4 PORTDbits.RD6
 
-void lcd_data(unsigned char data) // to pass character data to lcd
+
+// Function Declarations
+void WriteCommandToLCD(unsigned char);
+void WriteDataToLCD(char);
+void InitLCD(void);
+void WriteStringToLCD(const char*);
+void ClearLCDScreen(void);
+void keypad();
+
+//Program start from here
+int main(void)
 {
-    PORTC = data; // passing data to port C
-    RS=1; //this means that data passing will go to data register
-    RW=0; //to write data, this has to be 0
-    EN=1; //clock has to be enabled irrespective of process
-    __delay_ms(5);
-    EN=0; //disabling clock after 5 ms
-}
-
-void lcd_command(unsigned char cmd) //passing instruction
-{
-    PORTC = cmd; // passing instruction to port C
-    RS=0; //instruction passed to instruction register
-    RW=0; //to write data, this has to be 0
-    EN=1; //clock has to be enabled irrespective of process
-    ___delay_ms(5);
-    EN=0;  //disabling clock after 5 ms
-}
-
-void lcd_string(const unsigned char str, unsigned char num) //num=length of string
-{
-    unsigned char i;
-    for(i=0; i<num; i++){
-        lcd_data(str(i));
+    CMCON = 0x07;            // Turn comparator off
+    InitLCD();                // Initialize LCD in 8bit mode
+    
+    ClearLCDScreen();          // Clear LCD screen
+    //WriteStringToLCD(msg);  // Write Hello World on LCD
+    while(1)
+    {
+        keypad();
     }
+    return 0;
 }
 
-void lcd_initialise()
-{
-    lcd_command(0x38);//setting lcd in 16 columns and 2 rows model
-    lcd_command(0x06);//auto increment cursor of lcd to next element when current element is printed
-    lcd_command(0x0C); //lcd on and cursor off
-    lcd_command(0x01); //clear screen
-}
 void keypad()
 {
     //Column 1 sequence
@@ -68,19 +68,19 @@ void keypad()
     C3=0;
     
     if(R1==1){ //Column 1 = 1 and if Row 1=1 it means 1 is pressed
-        lcd_data('1');
+        WriteStringToLCD("1");
         while(R1==1); // this condition will be true until we release the button
     }
     if(R2==1){
-        lcd_data('4');
+        WriteStringToLCD("4");
         while(R2==1);
     }
     if(R3==1){
-        lcd_data('7');
+        WriteStringToLCD("7");
         while(R3==1);
     }
     if(R4==1){
-        lcd_data('*');
+        WriteStringToLCD("*");
         while(R4==1);
     }
     
@@ -90,19 +90,19 @@ void keypad()
     C3=0;
     
     if(R1==1){ 
-        lcd_data('2');
+        WriteStringToLCD("2");
         while(R1==1); 
     }
     if(R2==1){
-        lcd_data('5');
+        WriteStringToLCD("5");
         while(R2==1);
     }
     if(R3==1){
-        lcd_data('8');
+        WriteStringToLCD("8");
         while(R3==1);
     }
     if(R4==1){
-        lcd_data('0');
+        WriteStringToLCD("0");
         while(R4==1);
     }
     
@@ -112,35 +112,66 @@ void keypad()
     C3=1;
     
     if(R1==1){ 
-        lcd_data('3');
+        WriteStringToLCD("3");
         while(R1==1); 
     }
     if(R2==1){
-        lcd_data('6');
+        WriteStringToLCD("6");
         while(R2==1);
     }
     if(R3==1){
-        lcd_data('9');
+        WriteStringToLCD("9");
         while(R3==1);
     }
     if(R4==1){
-        lcd_data('#');
+        WriteStringToLCD("#");
         while(R4==1);
     }
 }
 
-void main(void)
+void ToggleEpinOfLCD(void)
 {
-    TRISC = 0x00; 
-    TRISD = 0x00;
-    TRISB = 0xF0;
-    //we are implementing row scan technique
-    lcd_initialise();
-    lcd_command(0x80); // first row first column of lcd
-    lcd_string("KEYPAD:", 7); //printing keypad
-    lcd_command(0xC0);  //second row first column of lcd
-    while(1)
+    LCD_E = 1;                // Give a pulse on E pin
+    __delay_us(E_Delay);      // so that LCD can latch the
+    LCD_E = 0;                // data from data bus
+    __delay_us(E_Delay);
+}
+void WriteCommandToLCD(unsigned char Command)
+{
+    LCD_RS = 0;               // It is a command
+    LCD_Data_Bus = Command;   // Write Command value on data bus
+    ToggleEpinOfLCD();
+}
+void WriteDataToLCD(char LCDChar)
+{
+    LCD_RS = 1;               // It is data
+    LCD_Data_Bus = LCDChar;   // Write Data value on data bus
+    ToggleEpinOfLCD();
+}
+void InitLCD(void)
+{
+    // Firstly make all pins output
+    LCD_E          = 0;      // E = 0
+    LCD_RS           = 0;      // D = 0
+    LCD_Data_Bus     = 0;      // CLK = 0
+    LCD_E_Dir        = 0;      // Make Output
+    LCD_RS_Dir       = 0;      // Make Output
+    LCD_Data_Bus_Dir = 0;      // Make Output
+    
+    WriteCommandToLCD(0x38);    //function set
+    WriteCommandToLCD(0x0c);    //display on,cursor off,blink off
+    WriteCommandToLCD(0x01);    //clear display
+    WriteCommandToLCD(0x06);    //entry mode, set increment
+}
+void WriteStringToLCD(const char *s)
+{
+    while(*s)
     {
-     keypad();
+        WriteDataToLCD(*s++);   // print first character on LCD
     }
+}
+void ClearLCDScreen(void)
+{
+    WriteCommandToLCD(0x01);    // Clear the screen
+    __delay_ms(2);              // Delay for cursor to return at zero position
 }
